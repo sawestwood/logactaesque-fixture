f4c4fd546de14b526c1f9dacb0d2c0421efda77f
sawestwood <steve.westwood@hexsaw.org.uk>
2015-01-02 22:26:41 +0000

    application.properties introduced

--- src/main/java/uk/org/hexsaw/logactaesque/fixture/service/impl/HttpClientFixtureService.java
+++ src/main/java/uk/org/hexsaw/logactaesque/fixture/service/impl/HttpClientFixtureService.java
@@ -3,6 +3,7 @@ package uk.org.hexsaw.logactaesque.fixture.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import uk.org.hexsaw.logactaesque.fixture.adapter.FixtureGameAdapter;
@@ -21,10 +22,13 @@ public class HttpClientFixtureService implements FixtureService {
 
     @Autowired
     private FixtureGameAdapter adapter;
+    
+    @Value("${gameplay.url}")
+    private String gamePlayUrl;
 
     public FixtureResult play(Fixture fixture) {
         Game game = adapter.to(fixture);
-        GameResult gameResult = template.postForObject("https://logactaesque-game.cfapps.io/game/play", game, GameResult.class);
+        GameResult gameResult = template.postForObject(this.gamePlayUrl, game, GameResult.class);
         log.info(gameResult.toString());
         return new FixtureResult(fixture, gameResult.getHomeGoals(), gameResult.getAwayGoals());
     }
